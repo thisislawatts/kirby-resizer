7db84049cd061d993cfb4b142a57b7e8c45e4e2e
Luke Watts <luke@thisis.la>
2014-07-02 17:13:23 +0100

    Adds an action hook for ajax requests via an api

--- assets/js/resizer.js
+++ assets/js/resizer.js
@@ -4,7 +4,9 @@
 
 		var _this = this;
 
-		_this.$resizable = $('[data-resizable]');
+		_this.$resizable = $('[data-resizable]'),
+		_this.$anchors = _this.$resizable.parent('a');
+
 		_this.setUp();
 		_this.config = window.KirbyResizerConfig || {};
 
@@ -18,7 +20,8 @@
 		var _this = this;
 
 		_this.$resizable.each(function() {
-			var $el = $(this);
+			var $el = $(this),
+				$anchor = $(this).parents('a').first();
 
 			if (!$el.data('originalWidth')) {
 				$el.data({
@@ -27,6 +30,14 @@
 			}
 
 			$el.resizable({
+				start: function(event, ui) {
+					$anchor.addClass('s__resizing');
+				},
+				stop: function(event, ui ) {
+					setTimeout(function() {
+						$anchor.removeClass('s__resizing');
+					}, 250 );
+				},
 				aspectRatio: true,
 				resize: function(event, ui) {
 					var scale = ( ui.size.width/ window.innerWidth ) * 100;
@@ -37,11 +48,22 @@
 		});
 
 		$('body').append('<button id="resizer--save">Save</button>')
+
+
+		// Images Block
+		_this.$anchors.on('click', function(evt) {
+			
+			if ( $(this).hasClass('s__resizing') )
+				evt.preventDefault();
+
+		})
+
 	}
 
 	KirbyResizer.prototype.save = function() {
 		var data = this.toJSON();
 		$.post( '/api', {
+			action: 'kirby.resizer',
 			path: this.config.path,
 			jsonString : data,
 		}, function(res) {
@@ -55,8 +77,10 @@
 
 		_this.$resizable.each(function() {
 			var $el = $(this),
-				scale = $el.data('scale') || 100;
-			obj[$el.data('filename')] = scale;
+				scale = $el.data('scale') || 100,
+				filename = $el.data('filename') || $el.attr('src');
+				 
+			obj[ filename ] = scale;
 		});
 		
 		return JSON.stringify(obj);
--- resizer.php
+++ resizer.php
@@ -1,17 +1,30 @@
 <?php if(!defined('KIRBY') && !isset($_POST['resizersecret'])) die('Direct access is not allowed');
 
-/* Save JSON to file */
+/* TODO: Fix ajax calls to resizer js */
+if ( r::is_ajax() && r::get('action') === 'kirby.resizer' ) {
 
+
+	
+	if ( KirbyResizer::saveToJSON( r::get('path'), r::get('jsonString') ) ) {
+		echo json_encode( array('status' => 'ok' ) );
+	} else {
+		echo json_encode( array('status' => 'error' ) );
+	}
+
+	exit();
+}
+
+/* Save JSON to file */
 
 class KirbyResizer {
 
-	function __construct() {
+	function __construct( ) {
 
-		$this->saveToJSON();
+		if (isset($_POST['jsonString']))
+			$this->saveToJSON();
 	}
 
-	function saveToJSON( ) {
-		$file = f::write( $_POST['path'] . '/_resizer.json', $_POST['jsonString'] );
+	function saveToJSON($path, $data) {
+		return f::write( $path . '/_resizer.json', $data ) > 0;
 	}
-
 }
